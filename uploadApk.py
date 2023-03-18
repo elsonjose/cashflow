980ab8134acecb91fc903f951958d46bd98ae82e
elsonjose <elsonjoseofficial@gmail.com>
2023-03-18 15:57:02 +0530

    WorkFlow Updated
    - Created workflow to build and push android app to firebase storage
    - Updated Readme

---
+++ uploadApk.py
@@ -0,0 +1,13 @@
+# using firebase-admin==5.2.0
+from firebase_admin import storage, credentials, initialize_app
+import sys
+
+initialize_app(credentials.Certificate('cashflow_cloud_secret.json'),{
+  'storageBucket': sys.argv[1],
+})
+
+bucket = storage.bucket()
+blob = bucket.blob('cashflow.apk')
+blob.upload_from_filename('app/build/outputs/apk/debug/app-debug.apk')
+blob.make_public()
+print(blob.public_url)
